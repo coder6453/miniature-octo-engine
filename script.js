bc349a143bfcda8aa5fe154b748e49beef8ce82c
coder6453 <91762474+coder6453@users.noreply.github.com>
2023-01-21 07:43:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-var num=prompt("his");done=false;x=window.location.href;for (var i=1; i<=num; i++){history.pushState(0, 0, i==num?x:i.toString());if(i==num){done=true}}if(done===true){alert("Success!\n "+window.location.href+" \nIs In Your His "+num+(num==1?" time.":" Times. "))}
+
